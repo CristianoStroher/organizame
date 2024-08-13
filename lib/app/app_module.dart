f864c978913bf5a621329c18e774f092a5fc28fb
Cristiano Stroher <120978880+CristianoStroher@users.noreply.github.com>
2024-08-12 23:22:21 -0300

    firestore

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:organizame/app/app_widget.dart';
@@ -22,6 +23,7 @@ class AppModule extends StatelessWidget {
       providers: [
         Provider<LoginValidators>(create: (_) => LoginValidators.instance), //injetando a instância da classe de validação de login
         Provider(create: (_) => FirebaseAuth.instance), //injetando a instância do firebase)
+        Provider(create: (_) => FirebaseFirestore.instance), //injetando a instância do firestore
         Provider(create: (_) => SqliteConnectionFactory(), //injetando a conexão com o banco de dados
           lazy: false,
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:organizame/app/app_module.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -7,6 +8,9 @@ Future<void> main() async {
   await Firebase.initializeApp(
   options: DefaultFirebaseOptions.currentPlatform,
 );
+
+final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
   runApp(const AppModule());
 }
 
