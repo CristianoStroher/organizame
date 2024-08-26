4839499e1b957372748b3ce9fea39b2c68fbb670
Cristiano Stroher <120978880+CristianoStroher@users.noreply.github.com>
2024-08-26 00:11:10 -0300

    ajustes

---
+++ home_module.dart
@@ -0,0 +1,29 @@
+import 'package:organizame/app/core/modules/organizame_module.dart';
+import 'package:organizame/app/modules/home/home_controller.dart';
+import 'package:organizame/app/modules/home/home_page.dart';
+import 'package:organizame/app/repositories/tasks/tasks_repository.dart';
+import 'package:organizame/app/repositories/tasks/tasks_repository_impl.dart';
+import 'package:organizame/app/services/tasks/tasks_service.dart';
+import 'package:organizame/app/services/tasks/tasks_service_impl.dart';
+import 'package:provider/provider.dart';
+
+class HomeModule extends OrganizameModule {
+  HomeModule()
+      : super(
+          bindings: [
+            Provider<TasksRepository>(
+              create: (context) => TasksRepositoryImpl(sqLiteConnectionFactory: context.read()),
+            ),
+            Provider<TasksService>(
+              create: (context) => TasksServiceImpl(tasksRepository: context.read()),
+            ),
+            ChangeNotifierProvider(
+              create: (context) => HomeController(tasksService: context.read()),
+            ),
+          ],
+          routers: {
+            '/home': (context) => HomePage(homeController: context.read()),
+            
+          },
+        );
+}
