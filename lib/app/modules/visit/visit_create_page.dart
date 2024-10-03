d405f9e3d9ab247d568730b856da40b9424769bf
Cristiano Stroher <120978880+CristianoStroher@users.noreply.github.com>
2024-10-03 00:21:54 -0300

    ajuste layout

--- lib/app/modules/visit/visit_create_page.dart
+++ lib/app/modules/visit/visit_create_page.dart
@@ -1,19 +1,14 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:organizame/app/core/ui/theme_extensions.dart';
 import 'package:organizame/app/core/widget/organizame_elevatebutton.dart';
 import 'package:organizame/app/core/widget/organizame_logo_movie.dart';
 import 'package:organizame/app/models/enviroment_object.dart';
-import 'package:organizame/app/modules/visit/visit_controller.dart';
 import 'package:organizame/app/modules/visit/widgets/environment_list.dart';
 import 'package:organizame/app/modules/visit/widgets/visit_header.dart';
 
 class VisitCreatePage extends StatelessWidget {
-
-
-   
   VisitCreatePage({
-    super.key,   
+    super.key,
   });
 
   @override
@@ -42,55 +37,40 @@ class VisitCreatePage extends StatelessWidget {
         ],
       ),
       body: Form(
-          child: LayoutBuilder(
-        builder: (context, constraints) => SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              minHeight: constraints.maxHeight,
-              minWidth: constraints.maxWidth,
-            ),
-            child: IntrinsicHeight(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const VisitHeader(),
-                  const SizedBox(height: 10),
-                  EnvironmentList(
-                    environments: [
-                      EnvironmentObject(
-                        name: 'Ambiente 1',
-                        phone: '9999-9999',
-                        address: 'Rua 1, 123',
-                      ),
-                      EnvironmentObject(
-                        name: 'Ambiente 2',
-                        phone: '9999-9999',
-                        address: 'Rua 2, 123',
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  OrganizameElevatedButton(
-                    onPressed: () {},
-                    label: 'Salvar',
-                    textColor: const Color(0xFFFAFFC5),
-                  ),
-                  const SizedBox(height: 10),
-                  // OrganizameElevatedButton(
-                  //   onPressed: () {},
-                  //   label: 'Fechar',
-                  //   textColor: context.primaryColor,
-                  //   backgroundColor: context.scaffoldBackgroundColor,
-                  //   borderColor: context.primaryColor,
-                  // ),
-                ],
-              ),
+        child: LayoutBuilder(
+          builder: (context, constraints) => SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const VisitHeader(),
+                const SizedBox(height: 10),
+                EnvironmentList(
+                  environments: [
+                    EnvironmentObject(
+                      name: 'Ambiente 1',
+                      phone: '9999-9999',
+                      address: 'Rua 1, 123',
+                    ),
+                    EnvironmentObject(
+                      name: 'Ambiente 2',
+                      phone: '9999-9999',
+                      address: 'Rua 2, 123',
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                OrganizameElevatedButton(
+                  onPressed: () {},
+                  label: 'Salvar',
+                  textColor: const Color(0xFFFAFFC5),
+                ),
+                const SizedBox(height: 40),
+              ],
             ),
           ),
         ),
-      )),
+      ),
     );
   }
 }
