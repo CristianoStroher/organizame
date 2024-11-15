de2969b887b1230a2a8d668f88c6ea2030c4df03
Cristiano Stroher <120978880+CristianoStroher@users.noreply.github.com>
2024-11-14 21:39:35 -0300

    ajustado objeto imagens

---
+++ lib/app/models/enviroment_imagens.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+class EnviromentImagens {
+  
+  final String id;
+  final String filePath;
+  final DateTime creationDate;
+  final DateTime dateTime;
+  final String? description;
+  
+  EnviromentImagens({
+    required this.id,
+    required this.filePath,
+    required this.creationDate,
+    required this.dateTime,
+    this.description,
+  });
+
+  
+
+
+
+
+
+}
