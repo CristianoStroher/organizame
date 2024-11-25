009c785b5af73c9d64656931b0c54c162200bc5f
Cristiano Stroher <120978880+CristianoStroher@users.noreply.github.com>
2024-11-24 21:18:25 -0300

    ajustes budgets

--- lib/app/modules/homeBudgets/widgets/budgets.dart
+++ lib/app/modules/homeBudgets/widgets/budgets.dart
@@ -1,169 +1,177 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_overlay_loader/flutter_overlay_loader.dart';
+import 'package:intl/intl.dart';
+import 'package:logger/logger.dart';
 import 'package:organizame/app/core/ui/messages.dart';
 import 'package:organizame/app/core/ui/theme_extensions.dart';
-import 'package:organizame/app/models/technicalVisit_object.dart';
+import 'package:organizame/app/models/task_filter_enum.dart';
+import 'package:organizame/app/models/task_object.dart';
 import 'package:organizame/app/modules/homeBudgets/budgets_controller.dart';
+import 'package:organizame/app/modules/homeTasks/home_controller.dart';
+import 'package:organizame/app/modules/task/task_controller.dart';
+import 'package:organizame/app/modules/task/task_create_page.dart';
+import 'package:provider/provider.dart';
 
 class Budgets extends StatelessWidget {
-  final BudgetsController controller;
-  final BudgetsObject object;
-  final Function(TechnicalVisitObject) onEdit;
+  // final BudgetsController controller;
+  // final BudgetsObject object;
+  final dateFormatData = DateFormat('dd/MM/yyyy');
+  final dateFormatHora = DateFormat('HH:mm');
 
-  Visit({
+  Budgets({
     super.key,
-    required this.controller,
-    required this.object,
-    required this.onEdit,
+    // required this.object,
+    // required this.controller,
   });
-
-  Future<void> _confirmDelete(BuildContext context) async {
-    final scaffoldMessenger = ScaffoldMessenger.of(context);
-
-    final confirm = await showDialog<bool>(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text('Confirmar Exclusão', style: context.titleMedium),
-        content: Text('Deseja realmente excluir esta visita técnica?',
-            style: TextStyle(color: context.primaryColor, fontSize: 16)),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(false),
-            style: TextButton.styleFrom(
-              backgroundColor: Color(0xFFFAFFC5),
-              side: BorderSide(color: context.primaryColor, width: 1),
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-            ),
-            child: Text(
-              'Cancelar',
-              style: context.titleDefaut,
-            ),
-          ),
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(true),
-            style: TextButton.styleFrom(
-              backgroundColor: context.primaryColor,
-              side: BorderSide(color: context.primaryColor, width: 1),
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-            ),
-            child: Text(
-              'Excluir',
-              style: TextStyle(color: Color(0xFFFAFFC5), fontSize: 16),
-            ),
-          ),
-        ],
-      ),
-    );
-
-    if (confirm ?? false) {
-      try {
-        // await controller.deleteBudgets(object);
-        Messages.of(context).showInfo('Visita técnica excluída com sucesso');
-      } catch (e) {
-        Messages.of(context).showError('Erro ao excluir visita técnica');
-      }
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
-    final environmentCount = object.enviroment?.length ?? 0;
-    return Column(
-      children: [
-        Divider(
-          color: Colors.grey[300],
-          thickness: 1,
-          height: 1, // Reduzido para 1 para minimizar o espaço
-        ),
-        ListTile(
-          contentPadding:
-              const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-          title: Text(
-            object.customer?.name.toUpperCase() ?? 'Cliente não informado',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: context.primaryColor,
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => TaskCreatePage(
+              controller: controller,
+              task: object,
             ),
-            overflow: TextOverflow.ellipsis,
           ),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 4),
-              Row(
+        ).then((value) async {
+          await context.read<HomeController>().loadAllTasks();
+          await context
+              .read<HomeController>()
+              .findFilter(filter: TaskFilterEnum.today);
+        });
+      },
+      child: SizedBox(
+        child: Column(
+          children: [
+            Divider(
+              color: Colors.grey[300], // Linha cinza acima
+              thickness: 1.5,
+              height: 0,
+            ),
+            ListTile(
+              contentPadding: EdgeInsets.zero,
+              leading: Checkbox(
+                checkColor: context.primaryColor,
+                activeColor: context.primaryColorLight,
+                fillColor: WidgetStateProperty.all(context.primaryColorLight),
+                side: BorderSide(color: context.primaryColor, width: 1),
+                value: object.finalizado,
+                onChanged: (value) async {
+                  await context.read<HomeController>().finishTask(object);
+                  Logger().i('Tarefa finalizada: ${object.descricao}');
+                },
+              ),
+              title: Text(
+                object.descricao.toUpperCase(),
+                style: TextStyle(
+                  decoration:
+                      object.finalizado ? TextDecoration.lineThrough : null,
+                  fontFamily: context.titleDefaut.fontFamily,
+                  fontWeight: FontWeight.bold,
+                  color: context.primaryColor,
+                ),
+              ),
+              subtitle: Row(
                 children: [
+                  const SizedBox(height: 5),
                   Icon(
                     Icons.calendar_today,
                     size: 16,
                     color: context.secondaryColor,
                   ),
                   const SizedBox(width: 4),
-                  // Text(
-                  //   dateFormatData.format(object.date),
-                  //   style: context.titleDefaut,
-                  // ),
-                ],
-              ),
-              if (object.customer?.address?.isNotEmpty ?? false) ...[
-                const SizedBox(height: 4),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.location_on,
-                      size: 16,
+                  Text(
+                    dateFormatData.format(object.data),
+                    style: TextStyle(
+                      fontFamily: context.titleDefaut.fontFamily,
                       color: context.secondaryColor,
                     ),
-                    const SizedBox(width: 4),
-                    Expanded(
-                      child: Text(
-                        object.customer!.address!,
-                        style: context.titleDefaut,
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 2,
+                  ),                  
+                ],
+              ),
+              trailing: IconButton(
+                icon: Icon(Icons.delete, color: context.secondaryColor),
+                onPressed: () async {
+                  final bool? confirmDelete = await showDialog<bool>(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('Excluir tarefa', style: context.titleMedium),
+                      content: Text(
+                        'Deseja excluir a tarefa ${object.descricao}?',
+                        style: TextStyle(color: context.primaryColor),
                       ),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(false),
+                          style: TextButton.styleFrom(
+                            backgroundColor: context.primaryColorLight,
+                            side: BorderSide(
+                                color: context.primaryColor, width: 1),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 10),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                            ),
+                          ),
+                          child: Text('Cancelar', style: context.titleDefaut),
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(true),
+                          style: TextButton.styleFrom(
+                            backgroundColor: context.primaryColor,
+                            side: BorderSide(
+                                color: context.primaryColor, width: 1),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 10),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                            ),
+                          ),
+                          child: Text(
+                            'Excluir',
+                            style: TextStyle(
+                                color: context.primaryColorLight , fontSize: 16),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ],
-            ],
-          ),
-          trailing: SizedBox(
-            width: 96,
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  onPressed: () => onEdit(object),
-                  icon: Icon(
-                    Icons.edit,
-                    color: context.secondaryColor,
-                  ),
-                  tooltip: 'Editar orçamento',
-                ),
-                IconButton(
-                  onPressed: () => _confirmDelete(context),
-                  icon: Icon(
-                    Icons.delete,
-                    color: context.secondaryColor,
-                  ),
-                  tooltip: 'Excluir orçamento',
-                ),
-              ],
+                  );
+
+                  if (confirmDelete == true) {
+                    Loader.show(context);
+                    try {
+                      final result = //await controller.deleteTask(object);
+                          await context
+                              .read<HomeController>()
+                              .deleteTask(object);
+
+                      Loader.hide();
+                      if (result) {
+                        Messages.of(context)
+                            .showInfo('Tarefa excluída com sucesso');
+                      } else {
+                        Messages.of(context)
+                            .showError('Erro ao excluir tarefa');
+                      }
+                    } catch (e) {
+                      Loader.hide();
+                      Messages.of(context).showError('Erro ao excluir tarefa');
+                    }
+                  }
+                },
+              ),
             ),
-          ),
+            Divider(
+              color: Colors.grey[300], // Linha cinza abaixo
+              thickness: 1.5,
+              height: 0,
+            ),
+          ],
         ),
-        Divider(
-          color: Colors.grey[300],
-          thickness: 1,
-          height: 1, // Reduzido para 1 para minimizar o espaço
-        ),
-      ],
+      ),
     );
   }
 }
